4968df833e57aef0ff003b2d410ff248c1ecaddb
gukka <guka245@gmail.com>
2022-02-07 15:14:59 +0200

    JS

--- is.js
+++ is.js
@@ -4,3 +4,64 @@ is.num = function (num) {
     }
     return false
 };
+is.nan = function (num) {
+    if (num == isNaN) {
+        return true
+    }
+    return false
+};
+is.str = function (num) {
+    if (typeof num === "string") {
+        return true
+    }
+    return false
+};
+is.bool = function (num) {
+    if (typeof num === true) {
+        return true
+    }
+    return false
+};
+is.undef = function (num) {
+    if (typeof num === undefined) {
+        return true
+    }
+    return false
+};
+is.def = function (num) {
+    if (typeof num === undefined) {
+        return true
+    }
+    return false
+};
+is.num = function (num) {
+    if (typeof num === "numing") {
+        return true
+    }
+    return false
+};
+is.num = function (num) {
+    if (typeof num === "numing") {
+        return true
+    }
+    return false
+};
+is.num = function (num) {
+    if (typeof num === "numing") {
+        return true
+    }
+    return false
+};
+is.num = function (num) {
+    if (typeof num === "numing") {
+        return true
+    }
+    return false
+};
+is.num = function (num) {
+    if (typeof num === "numing") {
+        return true
+    }
+    return false
+};
+
