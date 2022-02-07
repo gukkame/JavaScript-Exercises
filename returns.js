4218596b39d618e5ae6f77a67f2d838b56b6e697
gukka <guka245@gmail.com>
2022-02-07 12:41:11 +0200

    JS

--- returns.js
+++ returns.js
@@ -2,10 +2,11 @@ function id(p1) {
     return p1
 }
 function getLength(p1) {
-    p = [].concat(p1)
+    let p = [].concat(p1)
     i = p.length;
     if (i == 1) {
         i = p[0].length
     }
     return i
 }
+console.log(getLength([2, 21]))
