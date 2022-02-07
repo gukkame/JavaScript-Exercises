41c06fbd9726c75b707220fcbf994fbd58672a82
gukka <guka245@gmail.com>
2022-02-07 11:16:49 +0200

    JS

--- declarations.js
+++ declarations.js
@@ -2,16 +2,16 @@ const escapeStr = "`\\/\"'"
 const arr = [4, '2']
 
 const obj = {
-    str: String,
-    num: Number,
-    bool: Boolean,
+    str: 'String',
+    num: ' Number',
+    bool: 'Boolean',
     undef: undefined
 }
 const nested = {
     arr: arr,
     obj: { obj }
 }
-console.log(obj.str)
+console.log(typeof obj.str)
 
 Object.freeze(nested);
 Object.freeze(arr);
