2d0c9539ebafb6d122b80fb244878292c868bf06
gukka <guka245@gmail.com>
2022-02-07 19:47:05 +0200

    JS

--- collections.js
+++ collections.js
@@ -1,16 +1,16 @@
-const str = 'hello'
-const arr = [1, 2, 1, 3]
-const obj = { x: 45, y: 75, radius: 24 }
-const set = new Set()
-const map = new Map()
-set.add(1)
-set.add(2)
-set.add(1)
-set.add(3)
-map.set('a', 1)
-map.set('b', 2)
-map.set(3, 'c')
-map.set(4, 'd')
+// const str = 'hello'
+// const arr = [1, 2, 1, 3]
+// const obj = { x: 45, y: 75, radius: 24 }
+// const set = new Set()
+// const map = new Map()
+// set.add(1)
+// set.add(2)
+// set.add(1)
+// set.add(3)
+// map.set('a', 1)
+// map.set('b', 2)
+// map.set(3, 'c')
+// map.set(4, 'd')
 
 function arrToSet(arr) {
     return new Set(arr);
