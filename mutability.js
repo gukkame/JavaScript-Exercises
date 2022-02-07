5de29f507a52440a560dfff07c712d0d05207ea0
Gunta Kļava <guka245@gmail.com>
2022-02-07 12:30:02 +0200

    JS

--- mutability.js
+++ mutability.js
@@ -1,9 +1,9 @@
 
-const person = {
-    name: 'Rick',
-    age: 77,
-    country: 'US',
-}
+// const person = {
+//     name: 'Rick',
+//     age: 77,
+//     country: 'US',
+// }
 let clone1 = {
     ...person
 };
