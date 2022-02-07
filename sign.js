7929e2a13a179ca39e5978ae89c74cbc8777af84
gukka <guka245@gmail.com>
2022-02-07 14:19:13 +0200

    JS

--- sign.js
+++ sign.js
@@ -8,10 +8,11 @@ function sign(num) {
     }
 }
 function sameSign(num1, num2) {
-    if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)) {
+    if ((num1 >= 0 && num2 >= 0) || (num1 < 0 && num2 < 0)) {
         return true
     }
     else {
         return false
     }
 }
+console.log(sameSign(0, 0))
