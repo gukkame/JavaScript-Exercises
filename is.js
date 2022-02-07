70600526a0ce25e4899519b11ae805a6205a1aeb
gukka <guka245@gmail.com>
2022-02-07 18:21:38 +0200

    JS

--- is.js
+++ is.js
@@ -61,8 +61,8 @@ is.truthy = function (num) {
 
 is.falsy = function (num) {
     if (is.num(num) || is.nan(num) || is.str(num) || is.bool(num) || is.undef(num) || is.def(num) || is.arr(num) || is.obj(num) || is.fun(num)) {
-        return true
+        return false
     }
-    return false
+    return true
 };
 
