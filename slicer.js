4118154bc3d60fe837063ae33f38ccb25999abbb
gukka <guka245@gmail.com>
2022-02-08 12:48:08 +0200

    JS

--- slicer.js
+++ slicer.js
@@ -1,8 +1,5 @@
 function slice(arr, start, end) {
     let result = [];
-    // let from = Math.max(start, 0);
-    // let to = Math.min(end);
-    // console.log(end)
     if (typeof end === "undefined") {
         end = arr.length
     }
@@ -12,19 +9,15 @@ function slice(arr, start, end) {
     if (end < 0) {
         end = arr.length + end
     }
-
-    // if ((!end) || (end > arr.length)) {
-    //     for (let i = from; i < arr.length; i++) {
-    //         result.push(arr[i]);
-    //     }
-    // } else {
-    // console.log(end)
-    // console.log(start)
     for (let i = start; i < end; i++) {
         result.push(arr[i]);
     }
 
-    return result.join("");
+    if (Array.isArray(arr)) {
+        return result
+    } else {
+        return result.join("");
+    }
 }
 console.log(slice('abcdef', -2))
 console.log(slice('abcdef', 0, 2))
