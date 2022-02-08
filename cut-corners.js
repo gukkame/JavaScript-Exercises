ac0b93eb0943fdac73c331bd00736198f21b3ab7
gukka <guka245@gmail.com>
2022-02-08 14:22:49 +0200

    JS

--- cut-corners.js
+++ cut-corners.js
@@ -1,25 +1,31 @@
 const num = [Math.PI, -Math.PI, Math.E, -Math.E, 0]
 
 function round(n) {
-
+    if (n > 0) {
+        n = n + 0.4
+    } else if (n < 0) {
+        n = n - 0.4
+    }
     return r(n, 0)
 }
-function ceil(n) {
-
-}
 function floor(n) {
-
+    return r(n, 0)
 }
 function trunc(num) {
-    num = num * 100
-    num = num / 100
-    return num
+    return r(num, 0)
 }
+function ceil(n) {
+    if (n > 0) {
+        n = n + 1
+    }
+    return r(n, 0)
+}
+
 function r(num, fixed) {
     var re = new RegExp('^-?\\d+(?:\.\\d{0,' + (fixed || -1) + '})?');
     return num.toString().match(re)[0];
 }
 console.log(num.map(round))
-// console.log(num.map(ceil))
-// console.log(num.map(floor))
+console.log(num.map(floor))
 console.log(num.map(trunc))
+console.log(num.map(ceil))
