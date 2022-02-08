d8291ea05fdb51ac62ac7e1f1637f3f3c294fda7
gukka <guka245@gmail.com>
2022-02-08 15:45:09 +0200

    JS

---
+++ nasa.js
@@ -0,0 +1,19 @@
+function nasa(n) {
+    let s = ""
+
+    for (let i = 1; i <= n; i++) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            s = s + "NASA" + " "
+        } else if (i % 3 == 0) {
+            s = s + "NA" + " "
+        } else if (i % 5 == 0) {
+            s = s + "SA" + " "
+        } else {
+            s = s + i.toString(10) + " ";
+        }
+    }
+
+    return s
+}
+console.log(nasa(15))
