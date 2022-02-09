d8af3cb26acc0bc9c63196353ff8d489580989e4
gukka <guka245@gmail.com>
2022-02-09 13:20:59 +0200

    JS

---
+++ vowel-dots.js
@@ -0,0 +1,24 @@
+function vowelDots(str) {
+    str1 = ""
+    let vowels = /[aeiou]/gi;
+    let result = str.match(vowels);
+    if (result == null) {
+        return str
+    }
+    for (let i = 0; i < str.length; i++) {
+        str1 += str[i]
+        for (let j = 0; j < result.length; j++) {
+
+            if (str[i] == result[j]) {
+                str1 += "."
+            }
+        }
+    }
+    return str1
+
+}
+
+console.log(vowelDots('something'));
+console.log(vowelDots(''));
+console.log(vowelDots('rhythm'))
+console.log(vowelDots('Algorithm'));
