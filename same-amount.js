a15a2c995772bbd79cc97d469500c7f5eed328c3
gukka <guka245@gmail.com>
2022-02-09 13:39:09 +0200

    JS

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,12 @@
 function sameAmount(str, r1, r2) {
     let result = str.match(r1);
     let result2 = str.match(r2);
+    if (result == null) {
+        return false
+    }
+    if (result2 == null) {
+        return false
+    }
     if (result.length == result2.length) {
         return true
     } else {
