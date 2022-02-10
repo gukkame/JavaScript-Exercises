b5142ef8cf35cbe7aec6ea7b59b3517309eb4cd6
gukka <guka245@gmail.com>
2022-02-10 21:21:10 +0200

    JS

---
+++ count-leap-years.js
@@ -0,0 +1,14 @@
+function countLeapYears(date) {
+    date = date.toString()
+    year = date.split(" ")
+    let y = year[3]
+    let p = 0
+    console.log(year[3]);
+
+    y /= 4.13
+    if (y < 0) {
+        return 0
+    }
+    return Math.round(y)
+}
+console.log(countLeapYears(new Date('2020-01-01')));
