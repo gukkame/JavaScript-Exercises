0d0c3537069076856626d4d6a39a1305af60c576
gukka <guka245@gmail.com>
2022-02-10 14:41:12 +0200

    JS

--- get-some-time.js
+++ get-some-time.js
@@ -1,6 +1,6 @@
 const firstDayWeek = (week, year) => {
     let d = new Date(year + '-01-01')
     if (week != 1)
-        d.setDate((week - 1) * 7 - d.getDate() + 2)
+        d.setDate((week - 1) * 7 - d.getDay() + 2)
     return d.toISOString().slice(0, 10).split('-').reverse().join('-')
 }
