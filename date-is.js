0f7932045756160fd7f61eec0964a398125fc1c8
gukka <guka245@gmail.com>
2022-02-10 14:40:41 +0200

    JS

--- date-is.js
+++ date-is.js
@@ -1,6 +1,5 @@
 function isValid(d) {
     return d instanceof Date && !isNaN(new Date(d)) || typeof d === 'number' && !isNaN(new Date(d));
-    // return d instanceof Date && !isNaN(d.getTime())
 }
 function isAfter(d1, d2) {
     if (!isValid(d1) || !isValid(d2)) {
@@ -38,33 +37,3 @@ function isPast(d) {
     }
     return false
 }
-
-// const invalid = (callback, ary = 1) => {
-//     for (const s of [
-//         `new Date('')`,
-//         `new Date(NaN)`,
-//         `''`,
-//         `'2013-01-01'`,
-//         `NaN`,
-//     ]) {
-//         if (callback(...Array(ary).fill(eval(s)))) {
-//             throw Error(`${callback.name}(${s}) should be false`)
-//         }
-//     }
-// }
-
-// console.log(invalid(isAfter, 2));
-// console.log(isFuture(new Date('1992-01-01')));
-// console.log(isFuture(new Date(Date.now() + 1)));
-// console.log(isFuture(new Date(2077, 11, 31)));
-
-
-
-// console.log(isBefore(new Date(2321, 11, 21), new Date(Date.now())));console.log(isBefore(123123, 526));
-// console.log(isBefore(new Date('1992-01-01'), new Date('1992-01-02')));
-// console.log(typeof Date.now());
-// console.log(typeof new Date('1995-12-17T03:24:00').getTime());
-// console.log(isValid(Date.now()));
-// console.log(isValid(new Date('December 17, 1995 03:24:00')));
-// console.log(isValid(new Date(1488370835081)));
-// console.log(isValid(new Date('1995-12-17T03:24:00').getTime()));
--- dr-strange.js
+++ dr-strange.js
@@ -13,11 +13,6 @@ function timeTravel(obj) {
     obj.date.setSeconds(obj.second)
     return obj.date
 }
-// console.log(addWeek(new Date('0001-01-06')));//saturday
-// console.log(addWeek(new Date('0001-01-11')));//secondThursday
-// console.log(addWeek(new Date('1664-08-09')));//saturday
-// console.log(addWeek(new Date('2048-12-07')));//monday
-
 
 console.log(timeTravel({
     date: new Date('2020-05-29 01:28:02'),
