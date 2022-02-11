091a1bf0728f77115563168430ddbc0bb2feac09
gukka <guka245@gmail.com>
2022-02-11 18:57:29 +0200

    JS

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,21 @@
+export function generateLetters() {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    for (let i = 0; i < 120; i++) {
+        let div = document.createElement("div")
+        let r = randomChars.charAt(Math.floor(Math.random() * randomChars.length))
+        document.body.append(div)
+        div.append(r)
+        div.id = i
+        document.getElementById(i).style.fontSize = (i + 11) + "px"
+        if (i < 40) {
+            document.getElementById(i).style.fontWeight = "300"
+        }
+        if (i >= 40 && i < 80) {
+            document.getElementById(i).style.fontWeight = "400"
+        }
+        if ( i >= 80) {
+            document.getElementById(i).style.fontWeight = "500"
+        }
+    }
+
+}
