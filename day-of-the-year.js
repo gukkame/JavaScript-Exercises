7426bf1bb40e85ee7615cde27cb15ee7650e0163
gukka <guka245@gmail.com>
2022-02-11 11:02:21 +0200

    JS

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,9 @@
 function dayOfTheYear(date) {
 
-    return  Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    let res = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24);
+    if (res < 0)
+        return 1
+    return res
 }
 
-console.log(dayOfTheYear(new Date('1664-08-09')));
+console.log(dayOfTheYear(new Date('0001-01-01')));
