0cc91d7b252d2c4483b954018df81a0966301263
gukka <guka245@gmail.com>
2022-02-11 20:32:05 +0200

    JS

---
+++ pimp-my-style.js
@@ -0,0 +1,21 @@
+import { styles } from './pimp-my-style.data.js'
+export function pimp() {
+    const btn = document.getElementsByTagName("button")
+
+    var classList = btn[0].classList;
+    if (classList.length == 16 || classList.contains("unpimp")) {
+        // console.log(classList.length);
+        // let s = styles.length - classList.length
+        // btn[0].classList.toggle("unpimp", classList.length > 0);
+        btn[0].classList.remove(classList[classList.length - 2]);
+
+    }
+    if (classList.length <= 16 && !classList.contains("unpimp")) {
+        let s = styles.length - classList.length
+        btn[0].classList.add(styles[14 - s]);
+        if (classList.length == 16) {
+            btn[0].classList.add("unpimp");
+        }
+    }
+
+}
