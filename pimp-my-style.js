bb325b751d010d86c292a0ba6eb6af96f24a79b9
gukka <guka245@gmail.com>
2022-02-11 20:34:33 +0200

    JS

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -4,9 +4,12 @@ export function pimp() {
 
     var classList = btn[0].classList;
     if (classList.length == 16 || classList.contains("unpimp")) {
-        // console.log(classList.length);
+        console.log(classList.length);
         // let s = styles.length - classList.length
         // btn[0].classList.toggle("unpimp", classList.length > 0);
+        if (classList.length == 2) {
+            btn[0].classList.remove(classList[1]);
+        }
         btn[0].classList.remove(classList[classList.length - 2]);
 
     }
