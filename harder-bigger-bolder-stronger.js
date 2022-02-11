97fb1070503cb00f909585d31f2ff52644c698bd
gukka <guka245@gmail.com>
2022-02-11 19:06:21 +0200

    JS

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -1,12 +1,12 @@
 export function generateLetters() {
     var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    for (let i = 1; i <= 120; i++) {
+    for (let i = 0; i < 120; i++) {
         let div = document.createElement("div")
         let r = randomChars.charAt(Math.floor(Math.random() * randomChars.length))
         document.body.append(div)
         div.append(r)
         div.id = i
-        document.getElementById(i).style.fontSize = (i + 10) + "px"
+        document.getElementById(i).style.fontSize = (i + 11) + "px"
         if (i < 40) {
             document.getElementById(i).style.fontWeight = "300"
         }
