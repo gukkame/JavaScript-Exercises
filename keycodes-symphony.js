acbb49844f894ad3fe9f1e768c69b2f7ddab1de9
gukka <guka245@gmail.com>
2022-02-12 12:50:10 +0200

    JS

---
+++ keycodes-symphony.js
@@ -0,0 +1,23 @@
+export function compose() {
+    window.addEventListener("keydown", function (event) {
+        const body = document.querySelector('body')
+        let div = document.createElement("div");
+        div.textContent = `${event.key}`;
+        if (event.key === 'Backspace') {
+            let divlength = document.getElementsByTagName("div").length
+            let div = document.getElementsByTagName("div")
+            div[divlength - 1].remove();
+        } else if (event.key === 'Escape') {
+            document.querySelectorAll("div").forEach((item) => {
+                item.remove()
+            })
+        } else {
+            document.body.append(div)
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            console.log(randomColor);
+            div.style.backgroundColor = "#" + randomColor
+            div.classList.add("note");
+        }
+    }, true);
+
+}
