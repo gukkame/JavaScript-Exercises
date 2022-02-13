19accdc553d48ad4363fca0114e9590fccc8bbfa
gukka <guka245@gmail.com>
2022-02-13 16:41:01 +0200

    JS

---
+++ long-words.js
@@ -0,0 +1,11 @@
+function longWords(words) {
+    return words.every(word => word.length > 5)
+
+}
+function oneLongWord(words) {
+    return words.some(word => word.length >= 10)
+}
+function noLongWords(words) {
+    return words.every(word => word.length < 7)
+
+}
--- using-reduce.js
+++ using-reduce.js
@@ -5,34 +5,22 @@ function adder(array1, n = 0) {
 }
 function sumOrMul(array1, n) {
     const initialValue = 0;
-
     if (typeof n !== "undefined") {
-
         array1.unshift(n)
     } else {
         array1.unshift(null)
     }
     console.log(array1);
     let a = array1.reduce((previousValue, currentValue) => {
-        // console.log(array1);
-        console.log("previous ", previousValue);
-        console.log("current ", currentValue);
-
         let b
         if (currentValue % 2 == 0) {
-            // console.log("d ", previousValue * currentValue, initialValue);
             b = previousValue * currentValue, initialValue
         } else {
-            // console.log("C ", previousValue + currentValue, initialValue);
-
             b = previousValue + currentValue, initialValue
         }
-        console.log("B ", b);
-
         return b
     });
     array1.shift()
-    console.log(" ");
     return a
 }
 function funcExec(funcArray, n) {
